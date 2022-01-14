506a1d06d9e91823691565b105fae667d552eae4
Zacdgamer <95112012+Zacdgamer@users.noreply.github.com>
2022-01-14 14:09:02 -0500

    English to morse code translator

---
+++ README.py
@@ -0,0 +1,627 @@
+
+charList=""
+ 
+def dot():
+    global charList
+    charList+="o"
+def dash():
+    global charList
+    charList+="-"
+def breakk():
+    global charList
+    charList+=" "
+def breakkk():
+    global charList
+    charList+="  "
+         
+         
+def A():
+    dot()
+    dash()
+def B():
+    dash()
+    dot()
+    dot()
+    dot()
+def C():
+    dash()
+    dot()
+    dash()
+    dot()
+def D():
+    dash()
+    dot()
+    dot()
+def E():
+    dot()
+def F():
+    dot()
+    dot()
+    dash()
+    dot()
+def G():
+    dash()
+    dash()
+    dot()
+def H():
+    dot()
+    dot()
+    dot()
+    dot()
+def I():
+    dot()
+    dot()
+def J():
+    dot()
+    dash()
+    dash()
+    dash()
+def K():
+    dash()
+    dot()
+    dash()
+def L():
+    dot()
+    dash()
+    dot()
+    dot()
+def M():
+    dash()
+    dash()
+def N():
+    dash()
+    dot()
+def O():
+    dash()
+    dash()
+    dash()
+def P():
+    dot()
+    dash()
+    dash()
+    dot()
+def Q():
+    dash()
+    dash()
+    dot()
+    dash()
+def R():
+    dot()
+    dash()
+    dot()
+def S():
+    dot()
+    dot()
+    dot()
+def T():
+    dash()
+def U():
+    dot()
+    dot()
+    dash()
+def V():
+    dot()
+    dot()
+    dot()
+    dash()
+def W():
+    dot()
+    dash()
+    dash()
+def X():
+    dash()
+    dot()
+    dot()
+    dash()
+def Y():
+    dash()
+    dot()
+    dash()
+    dash()
+def Z():
+    dash()
+    dash()
+    dot()
+    dot()
+def o():
+    dash()
+    dash()
+    dash()
+    dash()
+    dash()
+def i():
+    dot()
+    dash()
+    dash()
+    dash()
+    dash()
+def ii():
+    dot()
+    dot()
+    dash()
+    dash()
+    dash()
+def iii():
+    dot()
+    dot()
+    dot()
+    dash()
+    dash()
+def iv():
+    dot()
+    dot()
+    dot()
+    dot()
+    dash()
+def v():
+    dot()
+    dot()
+    dot()
+    dot()
+    dot()
+def vi():
+    dash()
+    dot()
+    dot()
+    dot()
+    dot()
+def vii():
+    dash()
+    dash()
+    dot()
+    dot()
+    dot()
+def viii():
+    dash()
+    dash()
+    dash()
+    dot()
+    dot()
+def ix():
+    dash()
+    dash()
+    dash()
+    dash()
+    dot()
+def new_line():
+    breakk()
+    breakk()
+    A()
+    A()
+    breakk()
+def amp():
+    dot()
+    dash()
+    dot()
+    dot()
+    dot()
+def apos():
+    dot()
+    dash()
+    dash()
+    dash()
+    dash()
+def at_sign():
+    dot()
+    dash()
+    dash()
+    dot()
+    dash()
+    dot()
+def open_parentheses():
+    dash()
+    dot()
+    dash()
+    dash()
+    dot()
+def close_parentheses():
+    dash()
+    dot()
+    dash()
+    dash()
+    dot()
+    dash()
+def colon():
+    dash()
+    dash()
+    dash()
+    dot()
+    dot()
+    dot()
+def comma():
+    dash()
+    dash()
+    dot()
+    dot()
+    dash()
+    dash()
+def equal_sign():
+    dash()
+    dot()
+    dot()
+    dot()
+    dash()
+def exclaim_point():
+    dash()
+    dot()
+    dash()
+    dot()
+    dash()
+    dash()
+def period():
+    dot()
+    dash()
+    dot()
+    dash()
+    dot()
+    dash()
+def hyphen():
+    dash()
+    dot()
+    dot()
+    dot()
+    dot()
+    dash()
+def multiply_sign():
+    dash()
+    dot()
+    dot()
+    dash()
+def slash():
+    dash()
+    dot()
+    dot()
+    dash()
+    dot()
+def percent():
+    o()
+    dot()
+    dot()
+    dot()
+    dot()
+    dash()
+    
+def nathanFull():
+    N()
+    breakk()
+    A()
+    breakk()
+    T()
+    breakk()
+    H()
+    breakk()
+    A()
+    breakk()
+    N()
+    breakkk()
+    A()
+    breakk()
+    D()
+    breakk()
+    A()
+    breakk()
+    R()
+ 
+ 
+def userinput(user):
+    userList=list(user)
+    x=-1
+    for num in userList:
+        if "a" in num:
+            breakk()
+            A()
+            x=x+1
+        elif "A" in num:
+            breakk()
+            A()
+            x=x+1
+        elif "b" in num:
+            breakk()
+            B()
+            x=x+1
+        elif "B" in num:
+            breakk()
+            B()
+            x=x+1
+        elif "c" in num:
+            breakk()
+            C()
+            x=x+1
+        elif "C" in num:
+            breakk()
+            C()
+            x=x+1
+        elif "d" in num:
+            breakk()
+            D()
+            x=x+1
+        elif "D" in num:
+            breakk()
+            D()
+            x=x+1
+        elif "e" in num:
+            breakk()
+            E()
+            x=x+1
+        elif "E" in num:
+            breakk()
+            E()
+            x=x+1
+        elif "f" in num:
+            breakk()
+            F()
+            x=x+1
+        elif "F" in num:
+            breakk()
+            F()
+            x=x+1
+        elif "g" in num:
+            breakk()
+            G()
+            x=x+1
+        elif "G" in num:
+            breakk()
+            G()
+            x=x+1
+        elif "h" in num:
+            breakk()
+            H()
+            x=x+1
+        elif "H" in num:
+            breakk()
+            H()
+            x=x+1
+        elif "i" in num:
+            breakk()
+            I()
+            x=x+1
+        elif "I" in num:
+            breakk()
+            I()
+            x=x+1
+        elif "j" in num:
+            breakk()
+            J()
+            x=x+1
+        elif "J" in num:
+            breakk()
+            J()
+            x=x+1
+        elif "k" in num:
+            breakk()
+            K()
+            x=x+1
+        elif "K" in num:
+            breakk()
+            K()
+            x=x+1
+        elif "l" in num:
+            breakk()
+            L()
+            x=x+1
+        elif "L" in num:
+            breakk()
+            L()
+            x=x+1
+        elif "m" in num:
+            breakk()
+            M()
+            x=x+1
+        elif "M" in num:
+            breakk()
+            M()
+            x=x+1
+        elif "n" in num:
+            breakk()
+            N()
+            x=x+1
+        elif "N" in num:
+            breakk()
+            N()
+            x=x+1
+        elif "o" in num:
+            breakk()
+            O()
+            x=x+1
+        elif "O" in num:
+            breakk()
+            O()
+            x=x+1
+        elif "p" in num:
+            breakk()
+            P()
+            x=x+1
+        elif "P" in num:
+            breakk()
+            P()
+            x=x+1
+        elif "q" in num:
+            breakk()
+            Q()
+            x=x+1
+        elif "Q" in num:
+            breakk()
+            Q()
+            x=x+1
+        elif "r" in num:
+            breakk()
+            R()
+            x=x+1
+        elif "R" in num:
+            breakk()
+            R()
+            x=x+1
+        elif "s" in num:
+            breakk()
+            S()
+            x=x+1
+        elif "S" in num:
+            breakk()
+            S()
+            x=x+1
+        elif "t" in num:
+            breakk()
+            T()
+            x=x+1
+        elif "T" in num:
+            breakk()
+            T()
+            x=x+1
+        elif "u" in num:
+            breakk()
+            U()
+            x=x+1
+        elif "U" in num:
+            breakk()
+            U()
+            x=x+1
+        elif "v" in num:
+            breakk()
+            V()
+            x=x+1
+        elif "V" in num:
+            breakk()
+            V()
+            x=x+1
+        elif "w" in num:
+            breakk()
+            W()
+            x=x+1
+        elif "W" in num:
+            breakk()
+            W()
+            x=x+1
+        elif "x" in num:
+            breakk()
+            X()
+            x=x+1
+        elif "X" in num:
+            breakk()
+            X()
+            x=x+1
+        elif "y" in num:
+            breakk()
+            Y()
+            x=x+1
+        elif "Y" in num:
+            breakk()
+            Y()
+            x=x+1
+        elif "z" in num:
+            breakk()
+            Z()
+            x=x+1
+        elif "Z" in num:
+            breakk()
+            Z()
+            x=x+1
+        elif " " in num:
+            breakkk()
+            x=x+1
+        elif "0" in num:
+            breakk()
+            o()
+            x=x+1
+        elif "1" in num:
+            breakk()
+            i()
+            x=x+1
+        elif "2" in num:
+            breakk()
+            ii()
+            x=x+1
+        elif "3" in num:
+            breakk()
+            iii()
+            x=x+1
+        elif "4" in num:
+            breakk()
+            iv()
+            x=x+1
+        elif "5" in num:
+            breakk()
+            v()
+            x=x+1
+        elif "6" in num:
+            breakk()
+            vi()
+            x=x+1
+        elif "7" in num:
+            breakk()
+            vii()
+            x=x+1
+        elif "8" in num:
+            breakk()
+            viii()
+            x=x+1
+        elif "8" in num:
+            breakk()
+            viii()
+            x=x+1
+        elif "9" in num:
+            breakk()
+            ix()
+            x=x+1
+        elif "&" in num:
+            breakk()
+            amp()
+            x=x+1
+        elif "\'" in num:
+            breakk()
+            apos()
+            x=x+1
+        elif "@" in num:
+            breakk()
+            at_sign()
+            x=x+1
+        elif "(" in num:
+            breakk()
+            open_parentheses()
+            x=x+1
+        elif ")" in num:
+            breakk()
+            close_parentheses()
+            x=x+1
+        elif ":" in num:
+            breakk()
+            colon()
+            x=x+1
+        elif "," in num:
+            breakk()
+            comma()
+            x=x+1
+        elif "=" in num:
+            breakk()
+            equal_sign()
+            x=x+1
+        elif "!" in num:
+            breakk()
+            exclaim_point()
+            x=x+1
+        elif "." in num:
+            breakk()
+            period()
+            x=x+1
+        elif "-" in num:
+            breakk()
+            hyphen()
+            x=x+1
+        elif "*" in num:
+            breakk()
+            multiply_sign()
+            x=x+1
+        elif "/" in num:
+            breakk()
+            slash()
+            x=x+1
+        elif "%" in num:
+            breakk()
+            percent()
+            x=x+1
+loop=""
+while loop!="no":
+    user=input("Enter a word no punctuations only lowercase letters.(spaces are allowed)")
+    userinput(user)
+    print(charList)
+    loop=input("translate?, yes or no:")
+
